014bb06ee8a8688fa41bacbc8820ef9674f6896e
Sij <sijibamgboye@yahoo.com>
2021-09-20 12:25:04 +0100

    Completed task 0

--- project_1/Task0.py
+++ project_1/Task0.py
@@ -3,11 +3,11 @@ Read file into texts and calls.
 It's ok if you don't understand how to read files.
 """
 import csv
-with open('texts.csv', 'r') as f:
+with open('project_1/texts.csv') as f:
     reader = csv.reader(f)
     texts = list(reader)
 
-with open('calls.csv', 'r') as f:
+with open('project_1/calls.csv') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
@@ -19,4 +19,30 @@ Print messages:
 "First record of texts, <incoming number> texts <answering number> at time <time>"
 "Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
 """
+def retrieve_text_details(texts, i = 1):
 
+    incoming_number, answering_number, time = [i  for i in texts[i]]
+        
+    return incoming_number, answering_number, time
+
+incoming_number, answering_number, time = retrieve_text_details(texts)
+
+
+print(f'First record of texts, {incoming_number} texts {answering_number} at time {time} ')
+
+
+def retrieve_call_details(calls, i = 1):
+
+    i = len(calls) -1
+
+    incoming_number, answering_number, time, during  = [i  for i in calls[i]]
+    
+        
+    return incoming_number, answering_number, time, during
+
+
+incoming_number, answering_number, time, during = retrieve_call_details(calls)
+
+
+
+print(f'Last record of calls, {incoming_number} calls {answering_number} at time {time}, lasting {during} seconds')
